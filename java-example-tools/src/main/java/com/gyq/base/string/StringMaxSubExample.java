b2904e69ae6b323c88e5a836783afa1f073eb2bb
gaoyaqiu <30494613@qq.com>
2018-12-16 20:16:27 +0800

    String基础练习

---
+++ java-example-tools/src/main/java/com/gyq/base/string/StringMaxSubExample.java
@@ -0,0 +1,22 @@
+package com.gyq.base.string;
+
+/**
+ * 获取两个字符串中最大的相同子串.
+ *
+ * @author gaoyaqiu
+ */
+public class StringMaxSubExample {
+
+    public static void main(String[] args) {
+
+        String source = "hello worldhello worldhhh";
+        String target = "";
+        String result = getMaxSubString(source, "");
+        System.out.println(result);
+    }
+
+    private static String getMaxSubString(String source, String s) {
+        return null;
+    }
+
+}
