ad374f9435c2b600b80882cfbe53bfe974a896de
gaoyaqiu <30494613@qq.com>
2019-12-29 22:37:15 +0800

    up

---
+++ java-example-tools/src/main/java/com/gyq/base/array/IteratorRemoveDemo.java
@@ -0,0 +1,24 @@
+package com.gyq.base.array;
+
+import com.google.common.collect.Lists;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author gaoyaqiu
+ */
+public class IteratorRemoveDemo {
+
+    public static void main(String[] args) {
+        List<Integer> list = Lists.newArrayList(1, 5, 5, 5, 4);
+
+        Iterator it = list.iterator();
+        while (it.hasNext()) {
+            if ((int)it.next() == 5) {
+                it.remove();
+            }
+        }
+        System.out.println(list);
+    }
+}
