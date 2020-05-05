e0d49f2a1e121616490eca8035f386da11a1d787
qiu <gyq30494613@gmail.com>
2020-05-05 16:50:43 +0800

    新增ReentrantLock源码解析

---
+++ java-example-tools/src/main/java/com/gyq/base/aqs/AbstractOwnableSynchronizer.java
@@ -0,0 +1,23 @@
+package com.gyq.base.aqs;
+
+
+public abstract class AbstractOwnableSynchronizer implements java.io.Serializable {
+
+  private static final long serialVersionUID = 3737899427754241961L;
+
+  protected AbstractOwnableSynchronizer() {
+  }
+
+  /**
+   * 独占模式同步器的当前持有线程. transient关键字表示属性不参与序列化
+   */
+  private transient Thread exclusiveOwnerThread;
+
+  protected final void setExclusiveOwnerThread(Thread thread) {
+    exclusiveOwnerThread = thread;
+  }
+
+  protected final Thread getExclusiveOwnerThread() {
+    return exclusiveOwnerThread;
+  }
+}
